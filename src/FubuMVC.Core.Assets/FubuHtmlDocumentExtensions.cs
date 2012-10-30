ce5c22c350bc8b6ce8cc7513aec1f91f02fd592f
jeremydmiller <jeremydmiller@yahoo.com>
2012-10-30 14:33:21 -0500

    fixed nugets

---
+++ src/FubuMVC.Core.Assets/FubuHtmlDocumentExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FubuMVC.Core.UI;
+
+namespace FubuMVC.Core.Assets
+{
+    public static class FubuHtmlDocumentExtensions
+    {
+        public static void WriteAssetsToHead(this FubuHtmlDocument document)
+        {
+            document.Head.Append(document.WriteAssetTags());
+        }
+
+        public static void WriteScriptsToBody(this FubuHtmlDocument document)
+        {
+            document.Body.Append(document.WriteScriptTags());
+        }
+
+    }
+}
