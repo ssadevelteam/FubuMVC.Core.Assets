7bebb673cb0f4a2c06c6f12ec493464858a02e86
jfebres <jaime.febres@gmail.com>
2012-12-11 15:10:26 -0500

    Updated WarmUpSetsForCombinationPolicyTester.

--- src/FubuMVC.Core.Assets.Testing/WarmUpSetsForCombinationPolicyTester.cs
+++ src/FubuMVC.Core.Assets.Testing/WarmUpSetsForCombinationPolicyTester.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using FubuMVC.Core.Assets;
 using FubuMVC.Core.Runtime;
 using FubuTestingSupport;
@@ -13,7 +14,7 @@ namespace FubuMVC.Tests.Assets
         private AssetGraph _graph;
         private IEnumerable<string> _assetsForSetA;
         private IEnumerable<string> _assetsForSetB;
-
+        private IEnumerable<string> _assetsForSetC;
         protected override void beforeEach()
         {
             _graph = new AssetGraph();
@@ -21,16 +22,25 @@ namespace FubuMVC.Tests.Assets
             _assetsForSetA = new[] { "a-1.js", "a-2.js" };
             _assetsForSetB = new[] { "b-1.css", "b-2.css" };
 
+            _assetsForSetC = new[] {"c-1.js", "c-2.js", "c-3.css", "c-4.css"};
+
             _assetsForSetA.Each(x => _graph.AddToSet("setA", x));
             _assetsForSetB.Each(x => _graph.AddToSet("setB", x));
+            _assetsForSetC.Each(x => _graph.AddToSet("setC", x));
 
             _graph.CompileDependencies(null);
+
             MockFor<IAssetDependencyFinder>()
                 .Stub(x => x.CompileDependenciesAndOrder(new[] { "setA" }))
                 .Return(_assetsForSetA);
+
             MockFor<IAssetDependencyFinder>()
                 .Stub(x => x.CompileDependenciesAndOrder(new[] { "setB" }))
                 .Return(_assetsForSetB);
+
+            MockFor<IAssetDependencyFinder>()
+                .Stub(x => x.CompileDependenciesAndOrder(new[] { "setC" }))
+                .Return(_assetsForSetC);
 
             ClassUnderTest.Apply(null, null, _graph);
         }
@@ -38,8 +48,11 @@ namespace FubuMVC.Tests.Assets
         [Test]
         public void plans_for_sets_are_generated()
         {
-            MockFor<IAssetTagPlanCache>().AssertWasCalled(x => x.PlanFor(MimeType.Javascript, _assetsForSetA));
-            MockFor<IAssetTagPlanCache>().AssertWasCalled(x => x.PlanFor(MimeType.Css, _assetsForSetB));
+            MockFor<IAssetTagPlanCache>().AssertWasCalled(x => x.PlanFor(Arg<MimeType>.Is.Equal(MimeType.Javascript), Arg<IEnumerable<string>>.List.Equal(_assetsForSetA)));
+            MockFor<IAssetTagPlanCache>().AssertWasCalled(x => x.PlanFor(Arg<MimeType>.Is.Equal(MimeType.Css), Arg<IEnumerable<string>>.List.Equal(_assetsForSetB)));
+            MockFor<IAssetTagPlanCache>().AssertWasCalled(x => x.PlanFor(Arg<MimeType>.Is.Equal(MimeType.Javascript), Arg<IEnumerable<string>>.List.Equal(_assetsForSetC.Where(f => f.EndsWith(".js")))));
+            MockFor<IAssetTagPlanCache>().AssertWasCalled(x => x.PlanFor(Arg<MimeType>.Is.Equal(MimeType.Css), Arg<IEnumerable<string>>.List.Equal(_assetsForSetC.Where(f => f.EndsWith(".css")))));
+
         }
     }
 }
