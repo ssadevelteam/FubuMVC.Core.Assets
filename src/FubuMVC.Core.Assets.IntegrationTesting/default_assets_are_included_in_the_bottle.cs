1e41f73c5dfefeb96960c0c197285b8b94420f7d
jmarnold <rncodepoet@gmail.com>
2012-12-12 21:58:39 -0600

    Adds jquery.continuations OOTB

--- src/FubuMVC.Core.Assets.IntegrationTesting/default_assets_are_included_in_the_bottle.cs
+++ src/FubuMVC.Core.Assets.IntegrationTesting/default_assets_are_included_in_the_bottle.cs
@@ -1,21 +1,15 @@
-﻿using System.Diagnostics;
+﻿using System;
 using System.Net;
-using System.Reflection;
-using Bottles.PackageLoaders.Assemblies;
 using FubuMVC.Core.UI;
 using HtmlTags;
 using NUnit.Framework;
 using FubuTestingSupport;
-using System.Collections.Generic;
 
 namespace FubuMVC.Core.Assets.IntegrationTesting
 {
-
     [TestFixture]
     public class default_assets_are_included_in_the_bottle
     {
-
-
         [Test]
         public void fetch_jquery()
         {
@@ -30,6 +24,32 @@ namespace FubuMVC.Core.Assets.IntegrationTesting
             SelfHostHarness.Endpoints.Get<AssetEndpoint>(x => x.get_underscore())
                 .StatusCodeShouldBe(HttpStatusCode.OK)
                 .ScriptNames().ShouldHaveTheSameElementsAs("_content/scripts/underscore-min.js");
+        }
+
+        [Test]
+        public void fetch_jquery_form()
+        {
+            SelfHostHarness.Endpoints.Get<AssetEndpoint>(x => x.get_jquery_form())
+                .StatusCodeShouldBe(HttpStatusCode.OK)
+                .ScriptNames().ShouldHaveTheSameElementsAs("_content/scripts/jquery-1.8.2.min.js", "_content/scripts/jquery.form.js");
+        }
+
+        [Test]
+        public void fetch_jquery_continuations()
+        {
+            var scripts = SelfHostHarness.Endpoints.Get<AssetEndpoint>(x => x.get_continuations())
+                    .StatusCodeShouldBe(HttpStatusCode.OK)
+                    .ScriptNames(); 
+            
+            scripts.ShouldHaveTheSameElementsAs("_content/scripts/jquery-1.8.2.min.js", "_content/scripts/jquery.continuations.js");
+        }
+
+        [Test]
+        public void fetch_jquery_continuations_forms()
+        {
+            SelfHostHarness.Endpoints.Get<AssetEndpoint>(x => x.get_jquery_continuations_forms())
+                .StatusCodeShouldBe(HttpStatusCode.OK)
+                .ScriptNames().ShouldHaveTheSameElementsAs("_content/scripts/jquery-1.8.2.min.js", "_content/scripts/jquery.continuations.js", "_content/scripts/jquery.form.js", "_content/scripts/jquery.continuations.forms.js");
         }
     }
 
@@ -57,7 +77,29 @@ namespace FubuMVC.Core.Assets.IntegrationTesting
 
             return _document;
         }
-    }
 
-    
+        public HtmlDocument get_jquery_form()
+        {
+            _document.Asset("jquery.form.js");
+            _document.WriteAssetsToHead();
+
+            return _document;
+        }
+
+        public HtmlDocument get_continuations()
+        {
+            _document.Asset("continuations");
+            _document.WriteAssetsToHead();
+
+            return _document;
+        }
+
+        public HtmlDocument get_jquery_continuations_forms()
+        {
+            _document.Asset("jquery.continuations.forms.js");
+            _document.WriteAssetsToHead();
+
+            return _document;
+        }
+    }
 }
