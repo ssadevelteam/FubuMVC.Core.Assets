7a825f434a733a8aa05e4cee7dccb555d11d78aa
jmarnold <rncodepoet@gmail.com>
2013-01-08 10:09:55 -0700

    Updates the embedded jquery.continuation scripts to 1.0

--- src/FubuMVC.Core.Assets/Content/scripts/jquery.continuations.js
+++ src/FubuMVC.Core.Assets/Content/scripts/jquery.continuations.js
@@ -1,4 +1,4 @@
-﻿// jquery.continuations v0.9.16
+﻿// jquery.continuations v1.0.17
 //
 // Copyright (C)2011 Joshua Arnold, Jeremy Miller
 // Distributed Under Apache License, Version 2.0
@@ -7,37 +7,37 @@
 
 (function ($) {
 
-    "use strict";
-
-    // Sanity check of dependencies
-    if (typeof ($) !== 'function') {
-        throw 'jQuery.continuations: jQuery not found.';
-    }
-
     var CORRELATION_ID = 'X-Correlation-Id';
     var policies = [];
-
-    function theContinuation() {
+	
+	function theContinuation() {
         this.errors = [];
         this.success = false;
         this.refresh = false;
         this.correlationId = null;
-        this.options = {};
+        this.options = { };
     }
     theContinuation.prototype = {
         success: false,
         errors: [],
         refresh: false,
         correlationId: null,
-        options: {},
-        matchOnProperty: function (prop, predicate) {
-            return typeof (this[prop]) !== 'undefined' && predicate(this[prop]);
-        },
+		options: {},
+		matchOnProperty: function(prop, predicate) {
+			return typeof(this[prop]) !== 'undefined' && predicate(this[prop]);
+		},
         isCorrelated: function () {
-            return this.matchOnProperty('correlationId', function (id) {
-                return id != null;
-            });
-        }
+			return this.matchOnProperty('correlationId', function(id) {
+				return id != null;
+			});
+        },
+		eachError: function(action) {
+			if(!this.errors) return;
+			
+			for(var i = 0; i < this.errors.length; i++) {
+				action(this.errors[i]);
+			}
+		}
     };
 
     var refreshPolicy = function () {
@@ -57,12 +57,12 @@
             $.continuations.windowService.navigateTo(continuation.navigatePage);
         };
     };
-
+    
     var redirectPolicy = function () {
         this.matches = function (continuation) {
             // TODO -- Harden this against the proper statuses
-            return continuation.matchOnProperty('statusCode', function (c) { return c != 200; })
-                && continuation.matchOnProperty('response', function (r) { return r.getResponseHeader('Location'); });
+            return continuation.matchOnProperty('statusCode', function(c) { return c != 200; })
+                && continuation.matchOnProperty('response', function(r) { return r.getResponseHeader('Location'); });
         };
         this.execute = function (continuation) {
             var url = continuation.response.getResponseHeader('Location');
@@ -78,52 +78,52 @@
             $.continuations.trigger('ContinuationError', continuation);
         };
     };
-
+    
     var httpErrorPolicy = function () {
         this.matches = function (continuation) {
-            return continuation.matchOnProperty('statusCode', function (code) { return code != 200; });
+            return continuation.matchOnProperty('statusCode', function(code) { return code != 200; });
         };
         this.execute = function (continuation) {
             $.continuations.trigger('HttpError', continuation);
         };
     };
 
-    var continuations = function () {
+    var continuations = function () { 
         this.callbacks = {};
     };
     continuations.prototype = {
         // I'm calling YAGNI on the unbind since we have a reset
-        bind: function (topic, callback) {
-            if (!this.callbacks[topic]) {
+        bind: function(topic, callback) {
+            if( !this.callbacks[topic] ) {
                 this.callbacks[topic] = [];
             }
-
+            
             this.callbacks[topic].push(callback);
         },
         // Mostly public for testing
-        trigger: function (topic, payload, context) {
-            if (!payload) {
-                payload = {};
-            }
-
-            if (!this.callbacks[topic]) {
+        trigger: function(topic, payload, context) {
+			if(!payload) {
+				payload = {};
+			}
+			
+			if( !this.callbacks[topic] ) {
                 this.callbacks[topic] = [];
             }
-
-            if (!context) {
-                context = {
-                    topic: topic
-                };
-            }
+			
+			if(!context) {
+				context = {
+					topic: topic
+				};
+			}
 
             var actions = this.callbacks[topic];
-            for (var i = 0; i < actions.length; i++) {
-                actions[i].call(context, payload);
+            for(var i = 0; i < actions.length; i++) {
+				actions[i].call(context, payload);
             }
-
-            if (topic != '*') {
-                this.trigger('*', payload, { topic: topic });
-            }
+			
+			if(topic != '*') {
+				this.trigger('*', payload, {topic: topic});
+			}
         },
         init: function () {
             var self = this;
@@ -132,32 +132,32 @@
                     correlationId: xhr.getResponseHeader(CORRELATION_ID)
                 });
             });
-
+            
             $.ajaxSetup({
                 cache: false,
                 success: function (continuation, status, jqXHR) {
-                    var options = this.options;
-                    if (typeof (options) === 'undefined') {
-                        options = {};
-                    }
-                    if (typeof (continuation) !== 'undefined') {
-                        continuation.options = options;
-                    }
-
+					var options = this.options;
+					if(typeof(options) === 'undefined') {
+						options = {};
+					}
+					if(typeof(continuation) !== 'undefined') {
+						continuation.options = options;
+					}
+					
                     self.onSuccess({
                         continuation: continuation,
-                        callback: this.continuationSuccess,
+						callback: this.continuationSuccess,
                         status: status,
                         response: jqXHR
                     });
                 },
-                error: function (xhr, text, error) {
+                error: function(xhr, text, error) {
                     self.onError({
-                        response: xhr,
-                        text: text,
-                        error: error,
-                        callback: this.continuationError
-                    });
+						response: xhr, 
+						text: text,
+						error: error,
+						callback: this.continuationError
+					});
                 },
                 beforeSend: function (xhr, settings) {
                     self.setupRequest(xhr, settings);
@@ -173,29 +173,29 @@
             this.applyPolicy(new errorPolicy());
             this.applyPolicy(new httpErrorPolicy());
         },
-        onError: function (options) {
+        onError: function(options) {
             var continuation = this.buildError(options.response, options.text, options.error);
-            var process = true;
-            if ($.isFunction(options.callback)) {
-                process = !(options.callback(continuation) === false);
-            }
-
-            if (process) {
-                this.process(continuation);
-            }
+			var process = true;
+			if($.isFunction(options.callback)) {
+				process = !(options.callback(continuation) === false);
+			}
+			
+			if(process) {
+				this.process(continuation);
+			}
         },
-        buildError: function (response, text, error) {
+        buildError: function(response, text, error) {
             var continuation = new $.continuations.continuation();
             continuation.success = false;
-
-            var header = response.getResponseHeader('Content-Type');
+            
+			var header = response.getResponseHeader('Content-Type');
             if (header && header.indexOf('json') != -1) {
                 continuation = JSON.parse(response.responseText);
             }
-
+            
             continuation.response = response;
             continuation.statusCode = response.status;
-
+            
             return continuation;
         },
         onSuccess: function (msg) {
@@ -208,10 +208,10 @@
             continuation.statusCode = msg.response.status;
             continuation.response = msg.response;
             continuation.correlationId = msg.response.getResponseHeader('X-Correlation-Id');
-
-            if ($.isFunction(msg.callback)) {
-                msg.callback(continuation);
-            }
+			
+			if($.isFunction(msg.callback)) {
+				msg.callback(continuation);
+			}
 
             this.process(continuation);
         },
@@ -219,7 +219,7 @@
         setupRequest: function (xhr, settings) {
             // this could come from the ajax options
             var id = settings.correlationId;
-            if (typeof (id) === 'undefined') {
+            if (typeof(id) === 'undefined') {
                 id = new Date().getTime().toString();
             }
             xhr.setRequestHeader(CORRELATION_ID, id);
@@ -231,15 +231,15 @@
             policies.push(policy);
             return this;
         },
-        // Mostly for testing
-        reset: function () {
-            policies.length = 0;
-            this.setupDefaults();
+		// Mostly for testing
+		reset: function() {
+			policies.length = 0;
+			this.setupDefaults();
             this.callbacks = {};
-        },
+		},
         process: function (continuation) {
-            var standardContinuation = new $.continuations.continuation();
-            continuation = $.extend(standardContinuation, continuation);
+			continuation= $.continuations.create(continuation);
+
             var matchingPolicies = [];
             for (var i = 0; i < policies.length; ++i) {
                 var p = policies[i];
@@ -262,7 +262,7 @@
             window.location = url;
         }
     };
-
+    
     var module = new continuations();
     module.init();
 
@@ -270,6 +270,10 @@
     // Exports
     $.continuations = module;
     $.continuations.fn = continuations.prototype;
-    $.continuations.continuation = theContinuation;
+	$.continuations.continuation = theContinuation;
+	$.continuations.create = function(values) {
+		var continuation = new theContinuation();
+		return $.extend(true, continuation, values);
+	};
 
 } (jQuery));
